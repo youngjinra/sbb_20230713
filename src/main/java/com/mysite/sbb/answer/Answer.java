c03e748f52c53496f6e3b875bfba54edcb665eac
ryj913 <ryj913@naver.com>
2023-07-13 16:21:22 +0900

    chapter3_complete

--- src/main/java/com/mysite/sbb/answer/Answer.java
+++ src/main/java/com/mysite/sbb/answer/Answer.java
@@ -1,11 +1,13 @@
 package com.mysite.sbb.answer;
 
 import com.mysite.sbb.question.Question;
+import com.mysite.sbb.user.SiteUser;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -21,4 +23,13 @@ public class Answer {
     private LocalDateTime createDate;
 
     @ManyToOne
-    private Question question;}
+    private Question question;
+
+    @ManyToOne
+    private SiteUser author;
+
+    private LocalDateTime modifyDate;
+
+    @ManyToMany
+    Set<SiteUser> voter;
+}
